1269a926d4762ec22d80ca9dbdc34463667f82c4
rafaelapcruz <79165115+rafaelapcruz@users.noreply.github.com>
2021-04-05 10:58:06 +0100

    index2 and externa js are optimizes and running

--- calcTemp.js
+++ calcTemp.js
@@ -1,55 +1,58 @@
+function calcKelvin(temp) {
+	if (document.getElementById('output_celsius').selected) {
+		return (temp - 273.15).toFixed(2) + ' degree Celsius';
+	} else if (document.getElementById('output_fahrenheit').selected) {
+		return (temp * 1.8 - 459.67).toFixed(2) + ' degree Fahrenheit';
+	} else if (document.getElementById('output_kelvin').selected) {
+		return temp + " Kelvin";
+	}
+}
+
+function calcCelsius(temp) {
+	if (document.getElementById('output_kelvin').selected) {
+		return (temp + 273.15).toFixed(2) + " Kelvin";
+	} else if (document.getElementById('output_fahrenheit').selected) {
+		return (temp * 1.8 + 32).toFixed(2) + " degree Fahrenheit";
+	} else if (document.getElementById('output_celsius').selected) {
+		return temp + " degree Celsius";
+	}
+}
+
+function calcFahrenheit(temp) {
+	if (document.getElementById('output_kelvin').selected) {
+		return ((temp + 459.67) / 1.8).toFixed(2) + " Kelvin";
+	} else if (document.getElementById('output_celsius').selected) {
+		return ((temp - 32) / 1.8).toFixed(2) + " degree Celsius";
+	} else if (document.getElementById('output_fahrenheit').selected) {
+		return temp + " degree Fahrenheit";
+	}
+}
+
 function calcTemp() {
 
-			var temp = parseFloat(document.getElementById("temperature").value);
-			var result;
-				if (document.getElementById('input_kelvin').selected) {
-					if (temp < 0) {
-						result = "Below absolute zero";
-					} else if (document.getElementById('output_celsius').selected) {
-						result = (temp - 273.15).toFixed(2);
-						result += ' degree Celsius';
-					} else if (document.getElementById('output_fahrenheit').selected) {
-						result = (temp * 1.8 - 459.67).toFixed(2);
-						result += ' degree Fahrenheit';
-					} else if (document.getElementById('output_kelvin').selected) {
-						result = temp + " Kelvin";
-					}
-				} else if (document.getElementById('input_celsius').selected) {
-					if (document.getElementById('output_kelvin').selected) {
-						result = (temp + 273.15).toFixed(2);
-						if (result < 0) {
-							result = "Below absolute zero";
-						} else {
-							result += " Kelvin";
-						}
-					} else if (document.getElementById('output_fahrenheit').selected) {
-						result = (temp * 1.8 + 32).toFixed(2);
-						if (result < -459.67) {
-							result = "Below absolute zero";
-						} else {
-							result += " degree Fahrenheit";
-						}
-					}
-				} else if (document.getElementById('input_fahrenheit').selected) {
-					if (document.getElementById('output_kelvin').selected) {
-						result = ((temp + 459.67) / 1.8).toFixed(2);
-						if (result < 0) {
-							result = "Below absolute zero";
-						} else {
-							result += " Kelvin";
-						}
-					} else if (document.getElementById('output_celsius').selected) {
-						result = ((temp - 32) / 1.8).toFixed(2);
-						if (result < -273.15) {
-							result = "Below absolute zero";
-						} else {
-							result += " degree Celsius";
-						}
-					}
-				} else {
-					result = "Choose the correspondent temperature scale";
-				}
-				
-				document.getElementById("answer").innerHTML = result;
+	var temp = parseFloat(document.getElementById("temperature").value);
+	var result;
+		if (document.getElementById('input_kelvin').selected) {
+			if (temp < 0) {
+				result = "Below absolute zero";
+			} else {
+				result = calcKelvin(temp);
 			}
-			
+		} else if (document.getElementById('input_celsius').selected) {
+			if (temp < -273.15) {
+				result = "Below absolute zero";
+			} else {
+				result = calcCelsius(temp);
+			}
+		} else if (document.getElementById('input_fahrenheit').selected) {
+			if (temp < -459.67) {
+				result = "Below absolute zero";
+			} else {
+				result = calcFahrenheit(temp);
+			}
+		} else {
+			result = "Choose the correspondent temperature scale";
+		}
+
+	document.getElementById("answer").innerHTML = result;
+}
