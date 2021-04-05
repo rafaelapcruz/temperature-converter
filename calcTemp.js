1a6e709690fab8cf513c3c133c7d92f524c053e4
rafaelapcruz <79165115+rafaelapcruz@users.noreply.github.com>
2021-04-05 12:15:52 +0100

    atualização das propriedades de CSS

--- calcTemp.js
+++ calcTemp.js
@@ -1,8 +1,8 @@
 function calcKelvin(temp) {
 	if (document.getElementById('output_celsius').selected) {
-		return (temp - 273.15).toFixed(2) + ' degree Celsius';
+		return (temp - 273.15).toFixed(2) + ' degrees Celsius';
 	} else if (document.getElementById('output_fahrenheit').selected) {
-		return (temp * 1.8 - 459.67).toFixed(2) + ' degree Fahrenheit';
+		return (temp * 1.8 - 459.67).toFixed(2) + ' degrees Fahrenheit';
 	} else if (document.getElementById('output_kelvin').selected) {
 		return temp + " Kelvin";
 	}
@@ -12,9 +12,9 @@ function calcCelsius(temp) {
 	if (document.getElementById('output_kelvin').selected) {
 		return (temp + 273.15).toFixed(2) + " Kelvin";
 	} else if (document.getElementById('output_fahrenheit').selected) {
-		return (temp * 1.8 + 32).toFixed(2) + " degree Fahrenheit";
+		return (temp * 1.8 + 32).toFixed(2) + " degrees Fahrenheit";
 	} else if (document.getElementById('output_celsius').selected) {
-		return temp + " degree Celsius";
+		return temp + " degrees Celsius";
 	}
 }
 
@@ -22,37 +22,41 @@ function calcFahrenheit(temp) {
 	if (document.getElementById('output_kelvin').selected) {
 		return ((temp + 459.67) / 1.8).toFixed(2) + " Kelvin";
 	} else if (document.getElementById('output_celsius').selected) {
-		return ((temp - 32) / 1.8).toFixed(2) + " degree Celsius";
+		return ((temp - 32) / 1.8).toFixed(2) + " degrees Celsius";
 	} else if (document.getElementById('output_fahrenheit').selected) {
-		return temp + " degree Fahrenheit";
+		return temp + " degrees Fahrenheit";
 	}
 }
 
 function calcTemp() {
 
 	var temp = parseFloat(document.getElementById("temperature").value);
+		//console.log(temp);
 	var result;
+	var input_scale;
 		if (document.getElementById('input_kelvin').selected) {
+			input_scale = " Kelvin"
 			if (temp < 0) {
 				result = "Below absolute zero";
 			} else {
 				result = calcKelvin(temp);
 			}
 		} else if (document.getElementById('input_celsius').selected) {
+			input_scale = " degrees Celsius"
 			if (temp < -273.15) {
 				result = "Below absolute zero";
 			} else {
 				result = calcCelsius(temp);
 			}
 		} else if (document.getElementById('input_fahrenheit').selected) {
+			input_scale = " degrees Fahrenheit"
 			if (temp < -459.67) {
 				result = "Below absolute zero";
 			} else {
 				result = calcFahrenheit(temp);
 			}
-		} else {
-			result = "Choose the correspondent temperature scale";
 		}
 
-	document.getElementById("answer").innerHTML = result;
+	document.getElementById("answer").innerHTML = temp + input_scale + " equals to " + result;
+	document.getElementById("answer-box").setAttribute("class", "btn");
 }
