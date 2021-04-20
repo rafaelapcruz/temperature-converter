6bdd7283e85cd2a5cc0562ce5469b81ebdd0737b
rafaelapcruz <79165115+rafaelapcruz@users.noreply.github.com>
2021-04-20 12:02:03 +0100

    added functional conditional dropdown

--- calcTemp.js
+++ calcTemp.js
@@ -1,53 +1,59 @@
-document.getElementById("input-scale").addEventListener("change", conditionalDropdown);
-
-document.getElementById("button").addEventListener("click", resetSecondDropdown);
-
 
 function conditionalDropdown() {
 	var inputScaleValue = document.getElementById("input-scale").value;
- 	var outputOptions = document.getElementsByClassName("output");
- 	console.log (inputScaleValue, outputOptions);
- 	for (var i = 0; i < outputOptions.length; i++) {
- 		if (outputOptions[i].value == inputScaleValue) {
- 			outputOptions[i].setAttribute("hidden", "");
- 		}
+ 	console.log (inputScaleValue);
+ 	var options = document.getElementById("output-scale");
+ 	switch (inputScaleValue) {
+ 		case "kelvin":
+ 			document.getElementById("output-scale").options[0]=new Option("Celsius","celsius");
+ 			document.getElementById("output-scale").options[1]=new Option("Fahrenheit","fahrenheit");
+ 			options[0].setAttribute('id', 'output-celsius');
+ 			options[1].setAttribute('id', 'output-fahrenheit');
+ 			break;
+
+ 		case "celsius":
+ 			document.getElementById("output-scale").options[0]=new Option("Kelvin","kelvin");
+ 			document.getElementById("output-scale").options[1]=new Option("Fahrenheit","fahrenheit");
+ 			options[0].setAttribute('id', 'output-kelvin');
+ 			options[1].setAttribute('id', 'output-fahrenheit');
+ 			break;
+ 		case "fahrenheit":
+ 			document.getElementById("output-scale").options[0]=new Option("Kelvin","kelvin");
+ 			document.getElementById("output-scale").options[1]=new Option("Celsius","celsius");
+ 			options[0].setAttribute('id', 'output-kelvin');
+ 			options[1].setAttribute('id', 'output-celsius');
+ 			break;
  	}
+ 	
 }
 
-function resetSecondDropdown() {
-	var outputOptions = document.getElementsByClassName("output");
-	console.log (outputOptions)
-	for (var i = 0; i < outputOptions.length; i++) {
-		outputOptions[i].removeAttribute("hidden"); //.setAttribute("class", "d-inline-block") or element.classList.remove("d-none");
-	}
-}
 
 function calcKelvin(temp) {
-	if (document.getElementById('output_celsius').selected) {
+	if (document.getElementById('output-celsius').selected) {
 		return (temp - 273.15).toFixed(2) + ' degrees Celsius';
-	} else if (document.getElementById('output_fahrenheit').selected) {
+	} else if (document.getElementById('output-fahrenheit').selected) {
 		return (temp * 1.8 - 459.67).toFixed(2) + ' degrees Fahrenheit';
-	} else if (document.getElementById('output_kelvin').selected) {
+	} else if (document.getElementById('output-kelvin').selected) {
 		return temp + " Kelvin";
 	}
 }
 
 function calcCelsius(temp) {
-	if (document.getElementById('output_kelvin').selected) {
+	if (document.getElementById('output-kelvin').selected) {
 		return (temp + 273.15).toFixed(2) + " Kelvin";
-	} else if (document.getElementById('output_fahrenheit').selected) {
+	} else if (document.getElementById('output-fahrenheit').selected) {
 		return (temp * 1.8 + 32).toFixed(2) + " degrees Fahrenheit";
-	} else if (document.getElementById('output_celsius').selected) {
+	} else if (document.getElementById('output-celsius').selected) {
 		return temp + " degrees Celsius";
 	}
 }
 
 function calcFahrenheit(temp) {
-	if (document.getElementById('output_kelvin').selected) {
+	if (document.getElementById('output-kelvin').selected) {
 		return ((temp + 459.67) / 1.8).toFixed(2) + " Kelvin";
-	} else if (document.getElementById('output_celsius').selected) {
+	} else if (document.getElementById('output-celsius').selected) {
 		return ((temp - 32) / 1.8).toFixed(2) + " degrees Celsius";
-	} else if (document.getElementById('output_fahrenheit').selected) {
+	} else if (document.getElementById('output-fahrenheit').selected) {
 		return temp + " degrees Fahrenheit";
 	}
 }
@@ -55,35 +61,35 @@ function calcFahrenheit(temp) {
 function calcTemp() {
 
 	var temp = parseFloat(document.getElementById("temperature").value);
-	var input_scale;
 	console.log(temp);
+	var scaleString;
 	var result;
 	if (isNaN(temp)) { //não é possível escrever var x === NaN; NaN is unwritable
 		alert("Temperature value must be a number.");
 	} else {
-		if (document.getElementById('input_kelvin').selected) {
-			input_scale = " Kelvin"
+		if (document.getElementById('input-kelvin').selected) {
+			scaleString = " Kelvin"
 			if (temp < 0) {
 				result = "Below absolute zero";
 			} else {
 				result = calcKelvin(temp);
 			}
-		} else if (document.getElementById('input_celsius').selected) {
-			input_scale = " degrees Celsius"
+		} else if (document.getElementById('input-celsius').selected) {
+			scaleString = " degrees Celsius"
 			if (temp < -273.15) {
 				result = "Below absolute zero";
 			} else {
 				result = calcCelsius(temp);
 			}
-		} else if (document.getElementById('input_fahrenheit').selected) {
-			input_scale = " degrees Fahrenheit"
+		} else if (document.getElementById('input-fahrenheit').selected) {
+			scaleString = " degrees Fahrenheit"
 			if (temp < -459.67) {
 				result = "Below absolute zero";
 			} else {
 				result = calcFahrenheit(temp);
 			}
 		}
-		document.getElementById("answer").innerHTML = temp + input_scale + " equals to " + result;
+		document.getElementById("answer").innerHTML = temp + scaleString + " equals to " + result;
 		document.getElementById("answer-box").setAttribute("class", "alert alert-warning");
 		document.getElementById("answer-box").setAttribute("role", "alert");
 	}
